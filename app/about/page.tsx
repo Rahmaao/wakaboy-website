bc6acf75d33dadaf02e99d31a57c9857f08decee
Rahmaao <rahma.olaniyan@aun.edu.ng>
2024-06-08 10:44:53 +0100

    feat: about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function AboutPage() {
   return (
-    <section id="about" className=" bg-[#FFFFFF] w-full min-h-[100vh]">
+    <section id="about" className=" bg-[#FFFFFF] w-full min-h-[100vh] pb-10">
       <div
         className="bg-[#001145] h-[40vh] md:h-[50vh] lg:h-[100vh]  flex justify-center items-center w-full bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/about.png')" }}
@@ -14,33 +14,33 @@ export default function AboutPage() {
           </p>
         </div>
       </div>
-      <div className="h-[150vh] flex flex-col justify-around">
+      <div className="h-[170vh] md:h-[100vh] lg:h-[150vh] flex flex-col justify-around">
         <div className=" md:h-[50vh] lg:h-[70vh] xl:h-[90vh] md:flex hidden justify-center items-center">
-          <div className="w-[60%] h-full text-[#001145] md:pl-14 xl:pl-20 flex flex-col justify-evenly">
-            <p className="text-left md:text-[30px] xl:text-[47px] md:leading-[56px] font-semibold">
+          <div className="w-[60%] h-full md:pl-14 xl:pl-20 flex flex-col justify-evenly">
+            <p className="text-left text-[#001145] text-[30px] md:text-[40px] xl:text-[47px] md:leading-[56px] font-semibold">
               Who are we?
             </p>
-            <p className=" w-[95%] text-left md:text-[15px] lg:text-[18px] xl:text-[21px]">
+            <p className=" w-[95%] text-[#010417] text-left md:text-[15px] lg:text-[18px] xl:text-[21px]">
               Wakaboy Express was founded by Ejike Chukwunyerenwa in 2019 with
               the aim of solving the struggle that small businesses face in
               finding reliable logistics partners. We aim to bridge that gap by
               providing trustworthy and efficient logistics solutions to
               businesses of all sizes.
               <br /> <br />
-               At Wakaboy Express, we are committed to
-              excellence and adaptability, providing tailored solutions that
-              enhance operational efficiency and customer satisfaction. Whether
-              you're a small business or a larger enterprise, our innovative
-              logistics support is designed to help your business thrive. Trust
-              Wakaboy Express to deliver the logistical reliability your
-              business needs to succeed.
+              At Wakaboy Express, we are committed to excellence and
+              adaptability, providing tailored solutions that enhance
+              operational efficiency and customer satisfaction. Whether you're a
+              small business or a larger enterprise, our innovative logistics
+              support is designed to help your business thrive. Trust Wakaboy
+              Express to deliver the logistical reliability your business needs
+              to succeed.
             </p>
           </div>
           <div className="w-[40%] h-full flex justify-end items-center">
             <img src="/rectangle.svg" alt="" />
           </div>
         </div>
-        <div className="min-h-[90vh] flex md:hidden justify-center items-center">
+        <div className="min-h-[95vh] flex md:hidden justify-center items-center">
           <div className="py-10 px-3">
             <div className="min-h-[30%] w-full flex">
               <div className=" text-[#001145]">
@@ -69,8 +69,26 @@ export default function AboutPage() {
             </p>
           </div>
         </div>
-        <div className="h-[25vh] md:h-[30vh] bg-blue-400"></div>
-        <div className="h-[25vh] md:h-[30vh] bg-teal-400"></div>
+        <div className="h-[35vh] md:h-[20vh] lg:h-[30vh] flex flex-col justify-evenly items-center">
+          <p className="text-center text-[#001145] text-[30px] md:text-[40px] xl:text-[47px] md:leading-[56px] font-semibold">
+            Our Vision
+          </p>
+          <p className="hidden md:block w-[95%] text-[#010417] lg:leading-[28px] text-center md:text-[15px] lg:text-[18px] xl:text-[21px]">
+          To revolutionize last-mile delivery in Lagos, becoming the trusted partner for businesses, <br /> and serving new standards of speed, reliability, and sustainability.
+          </p>
+          <p className="block md:hidden w-[95%] text-[#010417] text-center">
+          To revolutionize last-mile delivery in Lagos, becoming the trusted partner for businesses, and serving new standards of speed, reliability, and sustainability.
+          </p>
+        </div>
+        <div className="h-[35vh] md:h-[20vh] lg:h-[30vh] flex flex-col justify-evenly items-center">
+          <p className="text-center text-[#001145] text-[30px] md:text-[40px] xl:text-[47px] md:leading-[56px] font-semibold">
+            Our Mission
+          </p>
+          <p className="hidden md:block w-[95%] text-[#010417] lg:leading-[28px] text-center md:text-[15px] lg:text-[18px] xl:text-[21px]">
+          Our mission is to provide efficient and eco-friendly last-mile delivery solutions that enhance <br /> the lives of Lagosians and empower businesses to thrive          </p>
+          <p className="block md:hidden w-[95%] text-[#010417] text-center">
+          Our mission is to provide efficient and eco-friendly last-mile delivery solutions that enhance the lives of Lagosians and empower businesses to thrive          </p>
+        </div>
       </div>
     </section>
   );
