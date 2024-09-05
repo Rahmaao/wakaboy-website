1d8cbec3bfce4aa298ec2ec16546d76b4716eb5c
Rahmaao <rahma.olaniyan@aun.edu.ng>
2024-09-05 23:05:19 +0100

    footer edits

--- components/footer.tsx
+++ components/footer.tsx
@@ -51,7 +51,7 @@ export const Footer = () => {
               </p>
               <a
                 className="text-[#001145] text-sm lg:text-[16px] mb-2 pb-1 md:pb-2 flex items-center"
-                href="https://wa.me/2347088124733"
+                href="https://wa.me/09011279144"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +61,7 @@ export const Footer = () => {
                 >
                   <path d="M12.002 2.01c-5.52 0-10 4.483-10 10.002 0 1.767.467 3.465 1.347 4.983l-1.466 5.35 5.531-1.451c1.488.827 3.152 1.256 4.857 1.256 5.52 0 10-4.483 10-10.002 0-5.521-4.48-10.001-10-10.001zm4.837 14.382c-.25.699-1.212 1.25-1.984 1.381-.53.085-1.179.152-3.453-.727-2.898-1.2-4.766-4.146-4.917-4.337-.154-.191-1.178-1.573-1.178-3.001 0-1.429.75-2.134 1.018-2.424.268-.29.589-.365.79-.365s.396.003.567.009c.185.007.433-.069.68.524.25.605.852 2.084.927 2.235.073.15.121.326.023.526-.097.2-.146.325-.292.496-.145.172-.307.38-.437.511-.147.148-.299.309-.128.607.17.298.756 1.245 1.626 2.018 1.119 1.014 2.06 1.331 2.36 1.478.298.148.473.13.646-.085.173-.213.742-.866.941-1.164.195-.293.396-.246.67-.148.273.099 1.719.81 2.013.956.298.146.496.22.57.342.075.124.075.727-.176 1.425z" />
                 </svg>
-                +2347088124733
+                09011279144
               </a>
               <a
                 className="text-[#001145] text-sm lg:text-[16px] mb-2 pb-1 md:pb-2 flex items-center"
@@ -83,26 +83,7 @@ export const Footer = () => {
                 </svg>
                 wakaboyexpress@gmail.com
               </a>
-              {/* <a
-                className="text-[#001145] text-sm lg:text-[16px] mb-2 pb-1 md:pb-2 flex items-center"
-                href="mailto:wakaboyexpress@gmail.com"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-                wakaboyexpress@gmail.com
-              </a> */}
+
               <a
                 className="text-[#001145] text-sm lg:text-[16px] pb-1 md:pb-2 flex "
                 href="https://maps.google.com/?q=26+Olaniyi+street,+off+pedro+road,+Ladi-lak.+Bariga"
@@ -137,8 +118,8 @@ export const Footer = () => {
                 Newsletter Signup
               </p>
               <WaitlistForm2 />
-              <div className="w-[55%] md:w-[70%] lg:w-[60%] h-[20%] mt-4 flex justify-between items-center">
-                <a href="">
+              <div className="w-[25%] md:w-[30%] lg:w-[30%] h-[20%] mt-4 flex justify-between items-center">
+                {/* <a href="">
                   <Image
                     src="/fb.svg"
                     alt="Facebook"
@@ -146,8 +127,8 @@ export const Footer = () => {
                     height={32}
                     className="w-[24px] lg:w-[32px] lg:h-[32px] h-[24px]"
                   />
-                </a>
-                <a href="">
+                </a> */}
+                <a href="https://www.instagram.com/wakaboy.express?igsh=MmFmaml2a3diNWJt&utm_source=qr" target="_blank">
                   <Image
                     src="/ig.svg"
                     alt="Instagram"
@@ -156,7 +137,7 @@ export const Footer = () => {
                     className="w-[24px] lg:w-[32px] lg:h-[32px] h-[24px]"
                   />
                 </a>
-                <a href="">
+                <a href="https://www.linkedin.com/company/wakaboy-express/" target="_blank">
                   <Image
                     src="/in.svg"
                     alt="LinkedIn"
@@ -165,7 +146,7 @@ export const Footer = () => {
                     className="w-[24px] lg:w-[32px] lg:h-[32px] h-[24px]"
                   />
                 </a>
-                <a href="">
+                {/* <a href="">
                   <Image
                     src="/x.svg"
                     alt="Other Social Media"
@@ -173,7 +154,7 @@ export const Footer = () => {
                     height={32}
                     className="w-[24px] lg:w-[32px] lg:h-[32px] h-[24px]"
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
